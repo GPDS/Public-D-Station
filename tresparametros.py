1fcb8bb2720ceab213c4933e634b6eea1e5d4172
Rafael Duarte <duarte.rafael@academico.ifpb.edu.br>
2017-11-30 15:07:03 -0300

    tresparametros.py

--- tresparametros.py
+++ tresparametros.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#testetetetetsetsetse
+
 import pandas as pd                     #Package usado no trabalho com os arquivos .txt
 import numpy as np
 import matplotlib.pyplot as plt
