047232abd170b77b310ef0d63f5bb253a320868b
Rafael Duarte <duarte.rafael@academico.ifpb.edu.br>
2018-02-01 13:20:27 -0300

    --all

--- tresparametros.py
+++ tresparametros.py
@@ -8,6 +8,7 @@
 
 import pandas as pd                     #Package usado no trabalho com os arquivos .txt
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import re                               #Package padrão - Para extrair números da string
 import openpyxl                         #Package para trabalhar com os arquivos .xlsx
@@ -294,6 +295,124 @@ def Parameters_Plot():
     plt.show()
 #Plotagem dos gráficos de saída final - FIM
 
+#Inínio da Função do Bullseye
+def bullseye_plot(ax, data, segBold=None, cmap=None, norm=None):
+    """
+    Bullseye representation for the left ventricle.
+
+    Parameters
+    ----------
+    ax : axes
+    data : list of int and float
+        The intensity values for each of the 17 segments
+    segBold: list of int, optional
+        A list with the segments to highlight
+    cmap : ColorMap or None, optional
+        Optional argument to set the desired colormap
+    norm : Normalize or None, optional
+        Optional argument to normalize data into the [0.0, 1.0] range
+
+
+    Notes
+    -----
+    This function create the 17 segment model for the left ventricle according
+    to the American Heart Association (AHA) [1]_
+
+    References
+    ----------
+    .. [1] M. D. Cerqueira, N. J. Weissman, V. Dilsizian, A. K. Jacobs,
+        S. Kaul, W. K. Laskey, D. J. Pennell, J. A. Rumberger, T. Ryan,
+        and M. S. Verani, "Standardized myocardial segmentation and
+        nomenclature for tomographic imaging of the heart",
+        Circulation, vol. 105, no. 4, pp. 539-542, 2002.
+    """
+    if segBold is None:
+        segBold = []
+
+    linewidth = 2
+    data = np.array(data).ravel()
+
+    if cmap is None:
+        cmap = plt.cm.viridis
+
+    if norm is None:
+        norm = mpl.colors.Normalize(vmin=data.min(), vmax=data.max())
+
+    theta = np.linspace(0, 2*np.pi, 768)
+    r = np.linspace(0.2, 1, 4)
+
+    # Create the bound for the segment 17
+    for i in range(r.shape[0]):
+        ax.plot(theta, np.repeat(r[i], theta.shape), '-k', lw=linewidth)
+
+    # Create the bounds for the segments  1-12
+    for i in range(6):
+        theta_i = i*60*np.pi/180
+        ax.plot([theta_i, theta_i], [r[1], 1], '-k', lw=linewidth)
+
+    # Create the bounds for the segments 13-16
+    for i in range(4):
+        theta_i = i*90*np.pi/180 - 45*np.pi/180
+        ax.plot([theta_i, theta_i], [r[0], r[1]], '-k', lw=linewidth)
+
+    # Fill the segments 1-6
+    r0 = r[2:4]
+    r0 = np.repeat(r0[:, np.newaxis], 128, axis=1).T
+    for i in range(6):
+        # First segment start at 60 degrees
+        theta0 = theta[i*128:i*128+128] + 60*np.pi/180
+        theta0 = np.repeat(theta0[:, np.newaxis], 2, axis=1)
+        z = np.ones((128, 2))*data[i]
+        ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
+        if i+1 in segBold:
+            ax.plot(theta0, r0, '-k', lw=linewidth+2)
+            ax.plot(theta0[0], [r[2], r[3]], '-k', lw=linewidth+1)
+            ax.plot(theta0[-1], [r[2], r[3]], '-k', lw=linewidth+1)
+
+    # Fill the segments 7-12
+    r0 = r[1:3]
+    r0 = np.repeat(r0[:, np.newaxis], 128, axis=1).T
+    for i in range(6):
+        # First segment start at 60 degrees
+        theta0 = theta[i*128:i*128+128] + 60*np.pi/180
+        theta0 = np.repeat(theta0[:, np.newaxis], 2, axis=1)
+        z = np.ones((128, 2))*data[i+6]
+        ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
+        if i+7 in segBold:
+            ax.plot(theta0, r0, '-k', lw=linewidth+2)
+            ax.plot(theta0[0], [r[1], r[2]], '-k', lw=linewidth+1)
+            ax.plot(theta0[-1], [r[1], r[2]], '-k', lw=linewidth+1)
+
+    # Fill the segments 13-16
+    r0 = r[0:2]
+    r0 = np.repeat(r0[:, np.newaxis], 192, axis=1).T
+    for i in range(4):
+        # First segment start at 45 degrees
+        theta0 = theta[i*192:i*192+192] + 45*np.pi/180
+        theta0 = np.repeat(theta0[:, np.newaxis], 2, axis=1)
+        z = np.ones((192, 2))*data[i+12]
+        ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
+        if i+13 in segBold:
+            ax.plot(theta0, r0, '-k', lw=linewidth+2)
+            ax.plot(theta0[0], [r[0], r[1]], '-k', lw=linewidth+1)
+            ax.plot(theta0[-1], [r[0], r[1]], '-k', lw=linewidth+1)
+
+    # Fill the segments 17
+    if data.size == 17:
+        r0 = np.array([0, r[0]])
+        r0 = np.repeat(r0[:, np.newaxis], theta.size, axis=1).T
+        theta0 = np.repeat(theta[:, np.newaxis], 2, axis=1)
+        z = np.ones((theta.size, 2))*data[16]
+        ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
+        if 17 in segBold:
+            ax.plot(theta0, r0, '-k', lw=linewidth+2)
+
+    ax.set_ylim([0, 1])
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+
+#Fim da função do Bullseye
+
 
 #print("\033c") #Caso queira limpar o terminal
 #Início da abertura da planilha
@@ -488,8 +607,13 @@ while True:
         for colour_it in range(0,tcolunas3-2):
             print("APLAX:", colours[colour_it],":",txt3_s[colours[colour_it]].min(),"%")
             gls.append(txt3_s[colours[colour_it]].min())
+        print("Teste:", gls)
+
+        #Plotar antes de calcular a média para aproveitar a variável
+
         gls=np.mean(gls)
         print("\n\nGlobal Longitudinal Strain: ", gls,"%")
+
 
     elif prmt == "2":
         txt1_sliced_onsets = txt1[(txt1.index >= EMCvalues1[it-4]) & (txt1.index < EMCvalues2[it-4])]#Obtenção da Mechanical Dispersion
