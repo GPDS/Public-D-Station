8d93580b24620d3aeb9054d194616a1d9461a509
rafaelds9 <duarte.rafael@academico.ifpb.edu.br>
2018-09-18 15:35:08 -0300

    Parameters are now calculated at the same time.

--- D-Station.py
+++ D-Station.py
@@ -49,6 +49,7 @@ Avalues = []
 
 #Inicialiazação de Variáveis:
 it = 4   #Iterador para a marcação na planilha (Depende da linha inicial nela, nesse caso os valores estão a partir da linha 4)
+prmt = '0'
 
 #Função para ler apenas as P colunas do dataframe - INÍCIO
 def front(self, n):
@@ -576,10 +577,211 @@ def DR_bullseye(data):
     plt.show()
     #Fim da função do Bullseye
 
+def GLS_calc():             #Função para calculo do GLS
+    global txt1_s
+    global txt2_s
+    global txt3_s
+
+    if op == test_op:
+        txt1_s = txt1[(txt1.index >= MVCvalues1[0]) & (txt1.index < AVCvalues1[0])] #Valores até o AVC - Durante a sístole
+        txt2_s = txt2[(txt2.index >= MVCvalues1[0]) & (txt2.index < AVCvalues1[0])]
+        txt3_s = txt3[(txt3.index >= MVCvalues1[0]) & (txt3.index < AVCvalues1[0])]
+    else:
+        #Valores até o AVC - Durante a sístole
+
+        txt1_s = txt1[(txt1.index >= EMCvalues1[0]) & (txt1.index < AVCvalues1[0])]
+        txt2_s = txt2[(txt2.index >= EMCvalues1[0]) & (txt2.index < AVCvalues1[0])]
+        txt3_s = txt3[(txt3.index >= EMCvalues1[0]) & (txt3.index < AVCvalues1[0])]
+    #print((tcolunas1-2)+(tcolunas2-2)+(tcolunas3-2)) #quantidade total de segmentos
+    #print(list(txt2_s))                              #lista os nomes dos segmentos
+    #print (colours[:-2])
+    if prmt == '1':
+        print("\n\nPeak negative systolic strain:\n")
+    gls = []
+    colours=list(txt2_s)
+    for colour_it in range(0,tcolunas2-2):
+        if prmt == '1':
+            print("2CH:", colours[colour_it],":",txt2_s[colours[colour_it]].min(),"%")
+        gls.append(txt2_s[colours[colour_it]].min())
+    if prmt == '1':
+        print("\n")
+    colours=list(txt1_s)
+    for colour_it in range(0,tcolunas1-2):
+        if prmt == '1':
+            print("4CH:", colours[colour_it],":",txt1_s[colours[colour_it]].min(),"%")
+        gls.append(txt1_s[colours[colour_it]].min())
+    if prmt == '1':
+        print("\n")
+    colours=list(txt3_s)
+    for colour_it in range(0,tcolunas3-2):
+        if prmt == '1':
+            print("APLAX:", colours[colour_it],":",txt3_s[colours[colour_it]].min(),"%")
+        gls.append(txt3_s[colours[colour_it]].min())
+    gls=np.mean(gls)
+
+    if prmt == '1':
+        print("\n\n")
+    print("Global Longitudinal Strain: ", gls,"%")
+    if prmt != '0':
+        Parameters_Plot()
+
+def MD_calc():             #Função para calculo do MD
+
+    global txt1_sliced_onsets
+    global txt2_sliced_onsets
+    global txt3_sliced_onsets
+
+    if op == test_op:
+        txt1_sliced_onsets = txt1[(txt1.index >= LM_Time) & (txt1.index < RM_Time)]#Obtenção da Mechanical Dispersion
+        txt2_sliced_onsets = txt2[(txt2.index >= LM_Time) & (txt2.index < RM_Time)]
+        txt3_sliced_onsets = txt3[(txt3.index >= LM_Time) & (txt3.index < RM_Time)]
+    else:
+        txt1_sliced_onsets = txt1[(txt1.index >= EMCvalues1[0]) & (txt1.index < EMCvalues2[0])]#Obtenção da Mechanical Dispersion
+        txt2_sliced_onsets = txt2[(txt2.index >= EMCvalues1[0]) & (txt2.index < EMCvalues2[0])]
+        txt3_sliced_onsets = txt3[(txt3.index >= EMCvalues1[0]) & (txt3.index < EMCvalues2[0])]
+    global_minima_times = []
+    if prmt == '2':
+        print("\n\nTimes of peak negative strain:\n")
+
+    colours=list(txt2_sliced_onsets)
+    for colour_it in range(0,tcolunas2-2):
+        if prmt == '2':
+            if op == test_op:
+                print("2CH:", colours[colour_it],":",txt2_sliced_onsets[colours[colour_it]].idxmin(),"ms")
+            else:
+                print("2CH:", colours[colour_it],":",txt2_sliced_onsets[colours[colour_it]].idxmin()-EMCvalues1[0],"ms")
+        global_minima_times.append(txt2_sliced_onsets[colours[colour_it]].idxmin())
+    if prmt == '2':
+        print("\n")
+
+    colours=list(txt1_sliced_onsets)
+    for colour_it in range(0,tcolunas1-2):
+        if prmt == '2':
+            if op == test_op:
+                print("4CH:", colours[colour_it],":",txt1_sliced_onsets[colours[colour_it]].idxmin(),"ms")
+            else:
+                print("4CH:", colours[colour_it],":",txt1_sliced_onsets[colours[colour_it]].idxmin()-EMCvalues1[0],"ms")
+        global_minima_times.append(txt1_sliced_onsets[colours[colour_it]].idxmin())
+    if prmt == '2':
+        print("\n")
+
+    colours=list(txt3_sliced_onsets)
+    for colour_it in range(0,tcolunas3-2):
+        if prmt == '2':
+            if op == test_op:
+                print("APLAX:", colours[colour_it],":",txt3_sliced_onsets[colours[colour_it]].idxmin(),"ms")
+            else:
+                print("APLAX:", colours[colour_it],":",txt3_sliced_onsets[colours[colour_it]].idxmin()-EMCvalues1[0],"ms")
+        global_minima_times.append(txt3_sliced_onsets[colours[colour_it]].idxmin())
+
+    if prmt == '2':
+        print("\n\n")
+    print("Mechanical Dispersion: ",np.std(global_minima_times)*1000, "ms")
+    if prmt != '0':
+        Parameters_Plot()
+
+
+def DI_calc():             #Função para calculo do DI
+
+    global ThirdDiastoleTime
+    global txt1_dr
+    global txt2_dr
+    global txt3_dr
+
+    if op == test_op:
+        ThirdDiastoleTime = AVCvalues1[0]+(RM_Time-systolic_time)/3 #Tempo de AVC + tempo de diastole/3
+    else:
+        ThirdDiastoleTime = AVCvalues1[0]+((EMCvalues2[0]+Dif_LM_OnsetQRS1[0])-AVCvalues1[0])/3 #EMC2+Diferença do Pico R1 e o onset QRS1-AVC1
+    #Criação das células com os valores de AVC e de 1/3 da diástole - Início
+    a = np.full(tcolunas1, float('nan'))         #Cria uma linha de tcolunas NaN
+    txt1_dr = txt1
+    indices = list(txt1_dr.index.values)#Rotina para a alteração do índice da lista de forma que os novos indíces sejam o último índice
+    var_t_med = indices[len(txt1_dr)-1]/len(txt1_dr)    #Nativos da lista incrementados
+    txt1_dr.loc[AVCvalues1[0]] = a
+    txt1_dr.loc[ThirdDiastoleTime] = a
+    txt1_dr = txt1_dr.sort_index()
+    txt1_dr = txt1_dr.interpolate(method = 'linear') #Antes era cubic
+
+    a = np.full(tcolunas2, float('nan'))         #Cria uma linha de tcolunas NaN
+    txt2_dr = txt2
+    indices = list(txt2_dr.index.values)#Rotina para a alteração do índice da lista de forma que os novos indíces sejam o último índice
+    var_t_med = indices[len(txt2_dr)-1]/len(txt2_dr)    #Nativos da lista incrementados
+    txt2_dr.loc[AVCvalues1[0]] = a
+    txt2_dr.loc[ThirdDiastoleTime] = a
+    txt2_dr = txt1_dr.sort_index()
+    txt2_dr = txt2_dr.interpolate(method = 'linear')
+
+    a = np.full(tcolunas3, float('nan'))         #Cria uma linha de tcolunas NaN
+    txt3_dr = txt3
+    indices = list(txt3_dr.index.values)#Rotina para a alteração do índice da lista de forma que os novos indíces sejam o último índice
+    var_t_med = indices[len(txt3_dr)-1]/len(txt3_dr)    #Nativos da lista incrementados
+    txt3_dr.loc[AVCvalues1[0]] = a
+    txt3_dr.loc[ThirdDiastoleTime] = a
+    txt3_dr = txt3_dr.sort_index()
+    txt3_dr = txt3_dr.interpolate(method = 'linear')
+    #Criação das células com os valores de AVC e de 1/3 da diástole - Fim
+
+    if prmt == '3':
+        print("\n\nFirst third of diastole time: ",ThirdDiastoleTime*1000,"ms")
+
+    DI_2CH = []
+    colours=list(txt2_dr)
+    if prmt == '3':
+        print("\nDiastolic Recovery\n\n2CH:")
+    for colour_it in range(0,tcolunas2-2):
+        DI_2CH.append((txt2_dr.at[AVCvalues1[0],colours[colour_it]] - txt2_dr.at[ThirdDiastoleTime,colours[colour_it]])/txt2_dr.at[AVCvalues1[0],colours[colour_it]])
+        if prmt == '3':
+            print(colours[colour_it],":", DI_2CH[colour_it]*100,"%")
+
+    DI_4CH = []
+    colours=list(txt1_dr)
+    if prmt == '3':
+        print("\n4CH:")
+    for colour_it in range(0,tcolunas1-2):
+        DI_4CH.append((txt1_dr.at[AVCvalues1[0],colours[colour_it]] - txt1_dr.at[ThirdDiastoleTime,colours[colour_it]])/txt1_dr.at[AVCvalues1[0],colours[colour_it]])
+        if prmt == '3':
+            print(colours[colour_it],":", DI_4CH[colour_it]*100,"%")
+
+    DI_APLAX = []
+    colours=list(txt3_dr)
+    if prmt == '3':
+        print("\nAPLAX:")
+    for colour_it in range(0,tcolunas3-2):
+        DI_APLAX.append((txt3_dr.at[AVCvalues1[0],colours[colour_it]] - txt3_dr.at[ThirdDiastoleTime,colours[colour_it]])/txt3_dr.at[AVCvalues1[0],colours[colour_it]])
+        if prmt == '3':
+            print(colours[colour_it],":", DI_APLAX[colour_it]*100,"%")
+    if prmt == '3':
+        print("\n")
+    ALL_DI = [DI_2CH, DI_4CH, DI_APLAX]
+    ALL_DI100 = np.array(ALL_DI)*100
+
+    if op != test_op:
+
+        global BullseyeAux
+
+        BullseyeAux = [ALL_DI100[0][0], ALL_DI100[2][5], ALL_DI100[1][5], ALL_DI100[0][5], ALL_DI100[2][0], ALL_DI100[1][0], ALL_DI100[0][1], ALL_DI100[2][4], ALL_DI100[1][4], ALL_DI100[0][4],ALL_DI100[2][1], ALL_DI100[1][1], ALL_DI100[0][2], ALL_DI100[2][3], ALL_DI100[1][3], ALL_DI100[0][3], ALL_DI100[2][2], ALL_DI100[1][2]]
+        #O vetor APLAX está na ordem inversa.
+        if prmt == '3':
+            print("Segmentos Bullseye:\n")
+            for i in range(18):
+                print("Segmento ",i+1,"- Valor: ", BullseyeAux[i])
+
+        if prmt == '3':
+            print("\n\n")
+        print("ATENCAO: REVISAR ESSE VALOR -> Diastolic Index: ", np.std(ALL_DI))
+    else:
+        DI_op5 = []
+        DI_op5.extend(DI_2CH)
+        DI_op5.extend(DI_4CH)
+        DI_op5.extend(DI_APLAX)
+        if prmt == '3':
+            print("\n\n")
+        print("Diastolic Index: ", np.std(DI_op5))
+    if prmt != '0':
+        Parameters_Plot()
+
 
 #print("\033c") #Caso queira limpar o terminal
-
-#Início das edições
 
 #Início da abertura dos .txt
 
@@ -697,7 +899,6 @@ txt2.drop('Unnamed: 1', axis=1, inplace=True)
 txt3.drop('Unnamed: 1', axis=1, inplace=True)
 txt_mid.drop('Unnamed: 1', axis=1, inplace=True)
 #strain_rate_lv.drop('Unnamed: 1', axis=1, inplace=True)
-#Fim das edições
 
 tcolunas1=int(((txt1.size/len(txt1.index))))
 tcolunas2=int(((txt2.size/len(txt2.index))))
@@ -800,6 +1001,10 @@ else:
     print("Systolic Time: ", systolic_time*1000)
     print("Diastolic Time: ", (RM_Time - systolic_time)*1000)
     print("Ratio: Systolic Time/Diastolic Time: ",(systolic_time/(RM_Time - systolic_time)))
+
+GLS_calc()
+MD_calc()
+DI_calc()
 print("\n\n")
 #Trabalho com Excel - FIM
 
@@ -810,157 +1015,18 @@ while True:
     prmt = input("Parameter: ")
 
     if prmt == "1":                                                             #Obtenção do Global Longitudinal Strain
-        if op == test_op:
-            txt1_s = txt1[(txt1.index >= MVCvalues1[0]) & (txt1.index < AVCvalues1[0])] #Valores até o AVC - Durante a sístole
-            txt2_s = txt2[(txt2.index >= MVCvalues1[0]) & (txt2.index < AVCvalues1[0])]
-            txt3_s = txt3[(txt3.index >= MVCvalues1[0]) & (txt3.index < AVCvalues1[0])]
-        else:
-            txt1_s = txt1[(txt1.index >= EMCvalues1[0]) & (txt1.index < AVCvalues1[0])] #Valores até o AVC - Durante a sístole
-            txt2_s = txt2[(txt2.index >= EMCvalues1[0]) & (txt2.index < AVCvalues1[0])]
-            txt3_s = txt3[(txt3.index >= EMCvalues1[0]) & (txt3.index < AVCvalues1[0])]
-        #print((tcolunas1-2)+(tcolunas2-2)+(tcolunas3-2)) #quantidade total de segmentos
-        #print(list(txt2_s))                              #lista os nomes dos segmentos
-        #print (colours[:-2])
-        print("\n\nPeak negative systolic strain:\n")
-        gls = []
-        colours=list(txt2_s)
-        for colour_it in range(0,tcolunas2-2):
-            print("2CH:", colours[colour_it],":",txt2_s[colours[colour_it]].min(),"%")
-            gls.append(txt2_s[colours[colour_it]].min())
-        print("\n")
-        colours=list(txt1_s)
-        for colour_it in range(0,tcolunas1-2):
-            print("4CH:", colours[colour_it],":",txt1_s[colours[colour_it]].min(),"%")
-            gls.append(txt1_s[colours[colour_it]].min())
-        print("\n")
-        colours=list(txt3_s)
-        for colour_it in range(0,tcolunas3-2):
-            print("APLAX:", colours[colour_it],":",txt3_s[colours[colour_it]].min(),"%")
-            gls.append(txt3_s[colours[colour_it]].min())
-        gls=np.mean(gls)
-        print("\n\nGlobal Longitudinal Strain: ", gls,"%")
-
+        GLS_calc()
 
     elif prmt == "2":
-        if op == test_op:
-            txt1_sliced_onsets = txt1[(txt1.index >= LM_Time) & (txt1.index < RM_Time)]#Obtenção da Mechanical Dispersion
-            txt2_sliced_onsets = txt2[(txt2.index >= LM_Time) & (txt2.index < RM_Time)]
-            txt3_sliced_onsets = txt3[(txt3.index >= LM_Time) & (txt3.index < RM_Time)]
-        else:
-            txt1_sliced_onsets = txt1[(txt1.index >= EMCvalues1[0]) & (txt1.index < EMCvalues2[0])]#Obtenção da Mechanical Dispersion
-            txt2_sliced_onsets = txt2[(txt2.index >= EMCvalues1[0]) & (txt2.index < EMCvalues2[0])]
-            txt3_sliced_onsets = txt3[(txt3.index >= EMCvalues1[0]) & (txt3.index < EMCvalues2[0])]
-        global_minima_times = []
-        print("\n\nTimes of peak negative strain:\n")
-
-        colours=list(txt2_sliced_onsets)
-        for colour_it in range(0,tcolunas2-2):
-            if op == test_op:
-                print("2CH:", colours[colour_it],":",txt2_sliced_onsets[colours[colour_it]].idxmin(),"ms")
-            else:
-                print("2CH:", colours[colour_it],":",txt2_sliced_onsets[colours[colour_it]].idxmin()-EMCvalues1[0],"ms")
-            global_minima_times.append(txt2_sliced_onsets[colours[colour_it]].idxmin())
-        print("\n")
-
-        colours=list(txt1_sliced_onsets)
-        for colour_it in range(0,tcolunas1-2):
-            if op == test_op:
-                print("4CH:", colours[colour_it],":",txt1_sliced_onsets[colours[colour_it]].idxmin(),"ms")
-            else:
-                print("4CH:", colours[colour_it],":",txt1_sliced_onsets[colours[colour_it]].idxmin()-EMCvalues1[0],"ms")
-            global_minima_times.append(txt1_sliced_onsets[colours[colour_it]].idxmin())
-        print("\n")
-
-        colours=list(txt3_sliced_onsets)
-        for colour_it in range(0,tcolunas3-2):
-            if op == test_op:
-                print("APLAX:", colours[colour_it],":",txt3_sliced_onsets[colours[colour_it]].idxmin(),"ms")
-            else:
-                print("APLAX:", colours[colour_it],":",txt3_sliced_onsets[colours[colour_it]].idxmin()-EMCvalues1[0],"ms")
-            global_minima_times.append(txt3_sliced_onsets[colours[colour_it]].idxmin())
-
-        print("\n\nMechanical Dispersion: ",np.std(global_minima_times)*1000, "ms")
+        MD_calc()
 
     elif prmt == "3":
-        if op == test_op:
-            ThirdDiastoleTime = AVCvalues1[0]+(RM_Time-systolic_time)/3 #Tempo de AVC + tempo de diastole/3
-        else:
-            ThirdDiastoleTime = AVCvalues1[0]+((EMCvalues2[0]+Dif_LM_OnsetQRS1[0])-AVCvalues1[0])/3 #EMC2+Diferença do Pico R1 e o onset QRS1-AVC1
-        #Criação das células com os valores de AVC e de 1/3 da diástole - Início
-        a = np.full(tcolunas1, float('nan'))         #Cria uma linha de tcolunas NaN
-        txt1_dr = txt1
-        indices = list(txt1_dr.index.values)#Rotina para a alteração do índice da lista de forma que os novos indíces sejam o último índice
-        var_t_med = indices[len(txt1_dr)-1]/len(txt1_dr)    #Nativos da lista incrementados
-        txt1_dr.loc[AVCvalues1[0]] = a
-        txt1_dr.loc[ThirdDiastoleTime] = a
-        txt1_dr = txt1_dr.sort_index()
-        txt1_dr = txt1_dr.interpolate(method = 'linear') #Antes era cubic
-
-        a = np.full(tcolunas2, float('nan'))         #Cria uma linha de tcolunas NaN
-        txt2_dr = txt2
-        indices = list(txt2_dr.index.values)#Rotina para a alteração do índice da lista de forma que os novos indíces sejam o último índice
-        var_t_med = indices[len(txt2_dr)-1]/len(txt2_dr)    #Nativos da lista incrementados
-        txt2_dr.loc[AVCvalues1[0]] = a
-        txt2_dr.loc[ThirdDiastoleTime] = a
-        txt2_dr = txt1_dr.sort_index()
-        txt2_dr = txt2_dr.interpolate(method = 'linear')
-
-        a = np.full(tcolunas3, float('nan'))         #Cria uma linha de tcolunas NaN
-        txt3_dr = txt3
-        indices = list(txt3_dr.index.values)#Rotina para a alteração do índice da lista de forma que os novos indíces sejam o último índice
-        var_t_med = indices[len(txt3_dr)-1]/len(txt3_dr)    #Nativos da lista incrementados
-        txt3_dr.loc[AVCvalues1[0]] = a
-        txt3_dr.loc[ThirdDiastoleTime] = a
-        txt3_dr = txt3_dr.sort_index()
-        txt3_dr = txt3_dr.interpolate(method = 'linear')
-        #Criação das células com os valores de AVC e de 1/3 da diástole - Fim
-
-        print("\n\nFirst third of diastole time: ",ThirdDiastoleTime*1000,"ms")
-
-        DI_2CH = []
-        colours=list(txt2_dr)
-        print("\nDiastolic Recovery\n\n2CH:")
-        for colour_it in range(0,tcolunas2-2):
-            DI_2CH.append((txt2_dr.at[AVCvalues1[0],colours[colour_it]] - txt2_dr.at[ThirdDiastoleTime,colours[colour_it]])/txt2_dr.at[AVCvalues1[0],colours[colour_it]])
-            print(colours[colour_it],":", DI_2CH[colour_it]*100,"%")
-
-        DI_4CH = []
-        colours=list(txt1_dr)
-        print("\n4CH:")
-        for colour_it in range(0,tcolunas1-2):
-            DI_4CH.append((txt1_dr.at[AVCvalues1[0],colours[colour_it]] - txt1_dr.at[ThirdDiastoleTime,colours[colour_it]])/txt1_dr.at[AVCvalues1[0],colours[colour_it]])
-            print(colours[colour_it],":", DI_4CH[colour_it]*100,"%")
-
-        DI_APLAX = []
-        colours=list(txt3_dr)
-        print("\nAPLAX:")
-        for colour_it in range(0,tcolunas3-2):
-            DI_APLAX.append((txt3_dr.at[AVCvalues1[0],colours[colour_it]] - txt3_dr.at[ThirdDiastoleTime,colours[colour_it]])/txt3_dr.at[AVCvalues1[0],colours[colour_it]])
-            print(colours[colour_it],":", DI_APLAX[colour_it]*100,"%")
-        print("\n")
-        ALL_DI = [DI_2CH, DI_4CH, DI_APLAX]
-        ALL_DI100 = np.array(ALL_DI)*100
-
-        if op != test_op:
-            BullseyeAux = [ALL_DI100[0][0], ALL_DI100[2][5], ALL_DI100[1][5], ALL_DI100[0][5], ALL_DI100[2][0], ALL_DI100[1][0], ALL_DI100[0][1], ALL_DI100[2][4], ALL_DI100[1][4], ALL_DI100[0][4],ALL_DI100[2][1], ALL_DI100[1][1], ALL_DI100[0][2], ALL_DI100[2][3], ALL_DI100[1][3], ALL_DI100[0][3], ALL_DI100[2][2], ALL_DI100[1][2]]
-            #O vetor APLAX está na ordem inversa.
-            print("Segmentos Bullseye:\n")
-            for i in range(18):
-                print("Segmento ",i+1,"- Valor: ", BullseyeAux[i])
-            DR_bullseye(BullseyeAux)
-            #Ver a barra
-            print("\n\nDiastolic Index: ", np.std(ALL_DI))
-            print("\n\nATENCAO: REVISAR ESSE VALOR")
-        else:
-            DI_op5 = []
-            DI_op5.extend(DI_2CH)
-            DI_op5.extend(DI_4CH)
-            DI_op5.extend(DI_APLAX)
-            print("\n\nDiastolic Index: ", np.std(DI_op5))
-
+        DI_calc()
+        DR_bullseye(BullseyeAux)
+        #Ver a barra
     elif prmt == "4":
         print("\nPlot w/o any parameters")
-
+        Parameters_Plot()
 
     elif prmt == "0":
         break
@@ -968,5 +1034,4 @@ while True:
     else:
         print("\n\nInvalid option\n\n\n")
         continue
-    Parameters_Plot()
     print("\n")
